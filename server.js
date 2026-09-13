2fa8dbe588b5b649af0d4ca7e2c02bafdb39989e
Glakxic <ogglakxic@gmail.com>
2026-09-13 17:17:05 +0200

    fix js

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-// Middleware pour autoriser les requêtes CORS (si ton frontend est sur un autre domaine)
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// Middleware pour autoriser les requêtes CORS (si ton frontend est sur un autre domaine/port)
+app.use(require('cors')());
 
 // Route pour récupérer l'URL du webhook
 app.get('/api/webhook-url', (req, res) => {
-  res.json({ url: process.env.DISCORD_WEBHOOK_URL });
+  const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+  if (!webhookUrl) {
+    return res.status(500).json({ error: "URL de webhook non définie." });
+  }
+  res.json({ url: webhookUrl });
 });
 
-app.listen(3000, () => {
-  console.log('Serveur démarré sur http://localhost:3000');
+// Démarre le serveur
+const PORT = process.env.PORT || 3000 || 80 || 8000 || 443;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
